f662539435b53c8558ab37843193124cea31cf72
Sepehr <sepehrhashtroudi@gmail.com>
2022-08-31 02:12:55 -0600

    dictionary

--- Find_Focal_methods.py
+++ Find_Focal_methods.py
@@ -4,20 +4,21 @@ import os
 import sys
 from sklearn.model_selection import train_test_split
 import copy
+import json 
 
 src_path= "lang3/"
 method_names_path = "method_names/"
 methods_path = "methods/"
 context_path = "context/"
 
-if os.path.exists("src_lines.txt"):
-  os.remove("src_lines.txt")
-if os.path.exists("tests.txt"):
-  os.remove("tests.txt")
-if os.path.exists("test_path.txt"):
-  os.remove("test_path.txt")
-if os.path.exists("lines_covered.txt"):
-  os.remove("lines_covered.txt")
+# if os.path.exists("src_lines.txt"):
+#   os.remove("src_lines.txt")
+# if os.path.exists("tests.txt"):
+#   os.remove("tests.txt")
+# if os.path.exists("test_path.txt"):
+#   os.remove("test_path.txt")
+# if os.path.exists("lines_covered.txt"):
+#   os.remove("lines_covered.txt")
   
   
 X = []
@@ -35,6 +36,12 @@ if __name__ == "__main__":
     src_code = 0
     class_name = 0
     function_found_flag = 0
+    dataset = {}
+    path = ""
+    methods = ""
+    line_plus_method = ""
+    src_line_num = 0
+    
     for line in line2test:
       # print(line)
       
@@ -42,6 +49,7 @@ if __name__ == "__main__":
           if("Test" in line):
             print("test#########################################################")
           else:
+            line_src_dict = {}
             path = line.split("/")[12:]
             class_name = line.split("/")[-1].split(".")[0]
             # print(class_name)
@@ -54,28 +62,30 @@ if __name__ == "__main__":
             methods = f.readlines()
             f.close()
 
-            f = open("src_lines.txt","ab")
-            f.write(str.encode("/".join(path) + "\n"))
-            f.close()
 
-            f = open("tests.txt","ab")
-            f.write(str.encode("/".join(path) + "\n"))
-            f.close()
+            # f = open("src_lines.txt","ab")
+            # f.write(str.encode("/".join(path) + "\n"))
+            # f.close()
 
-            f = open("test_path.txt",'ab')
-            f.write(str.encode("<SRC>"+"/".join(path) + "\n"))
-            f.close()
+            # f = open("tests.txt","ab")
+            # f.write(str.encode("/".join(path) + "\n"))
+            # f.close()
 
-            f = open("lines_covered.txt",'ab')
-            f.write(str.encode("/".join(path) + "\n"))
-            f.close()    
+            # f = open("test_path.txt",'ab')
+            # f.write(str.encode("<SRC>"+"/".join(path) + "\n"))
+            # f.close()
+
+            # f = open("lines_covered.txt",'ab')
+            # f.write(str.encode("/".join(path) + "\n"))
+            # f.close()    
     
             methods2line = {}
             for method in methods:
-              line_num = method.split(str.encode("<line_num>:"))[1]
-              methods2line[int(line_num)] = method
+              method_line_num = method.split(str.encode("<line_num>:"))[1]
+              methods2line[int(method_line_num)] = method
       if "<line>" in line: 
         line_num = int(line.split(":")[1])-1
+        src_line_num = line_num
         src_line = src_code[line_num]
         nearest_func = 0
         diff = 50000
@@ -89,23 +99,26 @@ if __name__ == "__main__":
                 function_found_flag = 1
         
         # print("/".join(path))
-        f = open("src_lines.txt","ab")
+        # f = open("src_lines.txt","ab")
         if function_found_flag == 1:
-          f.write(str.encode(" [LINE] ") + src_line.replace(str.encode("\n"), str.encode(" ")).strip() + str.encode(" [LINE] ") + methods2line[nearest_func].split(str.encode("<line_num>:"))[0] + str.encode("\n"))
-          X.append(str.encode(" [LINE] ") + src_line.replace(str.encode("\n"), str.encode(" ")).strip() + str.encode(" [LINE] ") + methods2line[nearest_func].split(str.encode("<line_num>:"))[0] + str.encode("\n"))
-          f = open("lines_covered.txt",'ab')
-          f.write(str.encode(str(line_num+1) + "\n"))
-          f.close()
+          line_plus_method = str.encode(" [LINE] ") + src_line.replace(str.encode("\n"), str.encode(" ")).strip() + str.encode(" [LINE] ") + methods2line[nearest_func].split(str.encode("<line_num>:"))[0]
+          
+          # f.write(str.encode(" [LINE] ") + src_line.replace(str.encode("\n"), str.encode(" ")).strip() + str.encode(" [LINE] ") + methods2line[nearest_func].split(str.encode("<line_num>:"))[0] + str.encode("\n"))
+          # X.append(str.encode(" [LINE] ") + src_line.replace(str.encode("\n"), str.encode(" ")).strip() + str.encode(" [LINE] ") + methods2line[nearest_func].split(str.encode("<line_num>:"))[0] + str.encode("\n"))
+          # f = open("lines_covered.txt",'ab')
+          # f.write(str.encode(str(line_num+1) + "\n"))
+          # f.close()
         else:
-          pass
+          # print("method not found")
+          continue
           # f.write(src_line )
-        f.close()
+        # f.close()
 
 
 
       if "<test_name>" in line and function_found_flag == 1:
-        path = line.split("<###>")[0]
-        path = path.split(".")[3:-1]
+        test_path = line.split("<###>")[0]
+        test_path = test_path.split(".")[3:-1]
         # print(line.split(".")[-1])
         # print(line.split(".")[-1])
         
@@ -115,16 +128,16 @@ if __name__ == "__main__":
         for test_name in test_names:
           if class_name in test_name:
             selected_test_name = test_name.split(".")[-1].strip()+"()"
-            path = test_name.split(".")[3:-1]
+            test_path = test_name.split(".")[3:-1]
             # print("test_name ###########################################")
             break
-        f = open("/".join(path)+".java",'rb')
+        f = open("/".join(test_path)+".java",'rb')
         tests = f.read()
         f.close()
 
-        f = open("test_path.txt","ab")
-        f.write(str.encode("/".join(path)+".java" +  "\n"))
-        f.close()
+        # f = open("test_path.txt","ab")
+        # f.write(str.encode("/".join(test_path)+".java" +  "\n"))
+        # f.close()
 
         tests = tests.split(str.encode("@Test"))
         # print(tests)
@@ -140,71 +153,85 @@ if __name__ == "__main__":
               print(index)
               print("more than one test maped to one line")
               continue
-            f = open("tests.txt","ab")
-            f.write(str.encode("@Test") + test.replace( str.encode("\n"), str.encode(" [EOL] "))+str.encode("\n"))
+            line_src_dict[str(src_line_num+1)] = {"src": line_plus_method , "test":str.encode("@Test") + test.replace( str.encode("\n"), str.encode(" [EOL] ")),\
+            "test_path": str.encode("/".join(test_path)+".java")}
+            dataset["/".join(path)] = line_src_dict 
+            # f = open("tests.txt","ab")
+            # f.write(str.encode("@Test") + test.replace( str.encode("\n"), str.encode(" [EOL] "))+str.encode("\n"))
             Y.append(str.encode("@Test") + test.replace(str.encode("\n"), str.encode(" [EOL] "))+str.encode("\n"))
-            f.close()
+            # f.close()
         
         if index == 0: 
           print("no tests found for a line")
           print(selected_test_name)
-          print("/".join(path)+".java")
-          
-    with open("train.tests", "wb") as train_tests, open("train.methods", "wb") as train_methods , open("test.tests", "wb") as test_tests, open("test.methods", "wb") as test_methods:
-        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)    
-        for i in X_train:
-            train_methods.write(i)
-        for i in X_test:
-            test_methods.write(i)
-        for i in y_train:
-            train_tests.write(i)
-        for i in y_test:
-            test_tests.write(i)
-        
-    with open("test.methods", "rb") as test_methods, open("test.tests","rb") as test_tests,\
-      open("src_lines.txt","rb") as all_methods,\
-      open("test_sorted.methods","wb") as test_methods_with_address,\
-      open("test_sorted.tests","wb") as test_tests_with_address,\
-      open("test_path.txt","rb") as  test_path,\
-      open("test_path_sorted.txt","wb") as  test_path_sorted,\
-      open("lines_covered.txt","rb") as  lines_covered,\
-      open("lines_covered_test.txt","wb") as  lines_covered_test:
-        all_method_lines = all_methods.readlines()
-        test_method_lines = test_methods.readlines()
-        test_tests_lines = test_tests.readlines()
-        test_path_lines = test_path.readlines()
-        lines_covered_lines = lines_covered.readlines()
-        test_methods_sorted = []
-        test_tests_sorted = []
-        test_path_sorted_list = []
-        lines_covered_test_list = []
-        for aml_index, all_method_line  in enumerate(all_method_lines):
-          if str.encode(".java") in all_method_line:
-            test_methods_sorted.append(all_method_line)
-            test_tests_sorted.append(all_method_line)
-            test_path_sorted_list.append(str.encode("<SRC>: ") + all_method_line)
-            lines_covered_test_list.append(all_method_line)
-            continue
-          found = 0
-          for index, test_method_line in enumerate(test_method_lines):
-            if test_method_line == all_method_line:
-              test_tests_sorted.append(test_tests_lines[index])
-              test_methods_sorted.append(all_method_line)
-              test_path_sorted_list.append(test_path_lines[aml_index])
-              lines_covered_test_list.append(lines_covered_lines[aml_index])
-              test_method_lines.pop(index)
-              test_tests_lines.pop(index)
-              found =1
-              break
+          print("/".join(test_path)+".java")
 
-        for i in test_methods_sorted:
-          test_methods_with_address.write(i)
-        for i in test_tests_sorted:
-          test_tests_with_address.write(i)
-        for i in test_path_sorted_list:
-          test_path_sorted.write(i)
-        for i in lines_covered_test_list:
-          lines_covered_test.write(i)
+    # print(dataset)
+    f = open("new_src.methods","wb")
+    for key,value in dataset.items():
+      f.write(str.encode(key +  "\n"))
+      for line, _ in value.items():
+        f.write(str.encode(line +  "\n"))
+    f.close()
+    # out = json.dumps(dataset)
+    # with open('dataset.json', 'w') as f:
+    #   json.dump(out, f)
+
+    # with open("train.tests", "wb") as train_tests, open("train.methods", "wb") as train_methods , open("test.tests", "wb") as test_tests, open("test.methods", "wb") as test_methods:
+    #     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)    
+    #     for i in X_train:
+    #         train_methods.write(i)
+    #     for i in X_test:
+    #         test_methods.write(i)
+    #     for i in y_train:
+    #         train_tests.write(i)
+    #     for i in y_test:
+    #         test_tests.write(i)
+        
+    # with open("test.methods", "rb") as test_methods, open("test.tests","rb") as test_tests,\
+    #   open("src_lines.txt","rb") as all_methods,\
+    #   open("test_sorted.methods","wb") as test_methods_with_address,\
+    #   open("test_sorted.tests","wb") as test_tests_with_address,\
+    #   open("test_path.txt","rb") as  test_path,\
+    #   open("test_path_sorted.txt","wb") as  test_path_sorted,\
+    #   open("lines_covered.txt","rb") as  lines_covered,\
+    #   open("lines_covered_test.txt","wb") as  lines_covered_test:
+    #     all_method_lines = all_methods.readlines()
+    #     test_method_lines = test_methods.readlines()
+    #     test_tests_lines = test_tests.readlines()
+    #     test_path_lines = test_path.readlines()
+    #     lines_covered_lines = lines_covered.readlines()
+    #     test_methods_sorted = []
+    #     test_tests_sorted = []
+    #     test_path_sorted_list = []
+    #     lines_covered_test_list = []
+    #     for aml_index, all_method_line  in enumerate(all_method_lines):
+    #       if str.encode(".java") in all_method_line:
+    #         test_methods_sorted.append(all_method_line)
+    #         test_tests_sorted.append(all_method_line)
+    #         test_path_sorted_list.append(str.encode("<SRC>: ") + all_method_line)
+    #         lines_covered_test_list.append(all_method_line)
+    #         continue
+    #       found = 0
+    #       for index, test_method_line in enumerate(test_method_lines):
+    #         if test_method_line == all_method_line:
+    #           test_tests_sorted.append(test_tests_lines[index])
+    #           test_methods_sorted.append(all_method_line)
+    #           test_path_sorted_list.append(test_path_lines[aml_index])
+    #           lines_covered_test_list.append(lines_covered_lines[aml_index])
+    #           test_method_lines.pop(index)
+    #           test_tests_lines.pop(index)
+    #           found =1
+    #           break
+
+    #     for i in test_methods_sorted:
+    #       test_methods_with_address.write(i)
+    #     for i in test_tests_sorted:
+    #       test_tests_with_address.write(i)
+    #     for i in test_path_sorted_list:
+    #       test_path_sorted.write(i)
+    #     for i in lines_covered_test_list:
+    #       lines_covered_test.write(i)
 
            
     
--- evosuite_coverage_extract.py
+++ evosuite_coverage_extract.py
@@ -1,5 +1,7 @@
+from asyncio import open_unix_connection
 import xml.etree.ElementTree as ET
 import os
+import json
 
 if os.path.exists("lines_covered_evosuite.txt"):
    os.remove("lines_covered_evosuite.txt")
@@ -7,16 +9,32 @@ if os.path.exists("lines_covered_evosuite.txt"):
 
 tree = ET.parse('evosuite_reports/clover_lang.xml')
 root = tree.getroot()
+line_coverage = {}
 
 for file in root[0][1].findall('file'):
-    print(file.get("path"))
-    f = open("lines_covered_evosuite.txt",'a')
-    f.write(file.get("path")+"\n")
-    f.close()
-    for line in file.findall("line"):
-        print(line.get("num"))
-        f = open("lines_covered_evosuite.txt",'a')
-        f.write(line.get("num")+"\n")
-        f.close()
+    # print(file.get("path"))
+    # f = open("lines_covered_evosuite.txt",'a')
+    # f.write(file.get("path")+"\n")
+    # f.close()
     
+    lines = []
+    for line in file.findall("line"):
+        # print(line.get("num"))
+        # f = open("lines_covered_evosuite.txt",'a')
+        # f.write(line.get("num")+"\n")
+        # f.close()
+        lines.append(line.get("num"))
+    # print(lines)
+    if len(lines) > 0:
+        line_coverage["/".join(file.get("path").split('/')[10:])] = lines
+
+out = json.dumps(line_coverage)
+# print(out)
+with open('lines_covered_evosuite.json', 'w') as f:
+    json.dump(out, f)
+
+with open('lines_covered_evosuite.json', 'r') as f:
+   jsonObject = json.load(f)
+jsonObject = json.loads(jsonObject)
+print(jsonObject["apache/commons/lang3/builder/ReflectionToStringBuilder.java"])
 
