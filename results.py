164340c82294747c483dcda57d29fd226cf37b33
Sepehr <sepehrhashtroudi@gmail.com>
2022-12-05 21:53:00 -0700

    last

--- results.py
+++ results.py
@@ -6,11 +6,11 @@ import json
 if os.path.exists("lines_covered_evosuite.txt"):
    os.remove("lines_covered_evosuite.txt")
 
-f = open("test_info.txt")
+f = open("./generated_datasets/lang3_test_info.txt")
 test_info = f.readlines()
 f.close()
 
-tree = ET.parse('model_gen_coverage/clover_lang2.xml')
+tree = ET.parse('model_gen_coverage/clover_lang_3.xml')
 root = tree.getroot()
 line_coverage = {}
 for package in root[0].findall('package'):
@@ -40,18 +40,23 @@ for package in root[0].findall('package'):
                 print("type: " + line.get("type"))
         # print(lines)
         if len(lines) > 0:
-            line_coverage["/".join(file.get("path").split('/')[12:])] = lines
+            line_coverage["/".join(file.get("path").split('/')[10:])] = lines
 
 
 test_line_num = 0
 covered_lines = 0
+print(line_coverage.keys())
 for line in test_info:
     file_path = line.split("<path>:")[-1].split("<test_path>:")[0].strip()
+    # print(file_path)
     line_num = line.split("<path>:")[0].split(":")[-1].strip()
     test_line_num += 1
     if file_path in line_coverage.keys():
         if line_num in line_coverage[file_path]:
             covered_lines += 1
+        else:
+            
+            print(line_coverage[file_path])
 print(covered_lines/test_line_num)
 # print(out)
 out = json.dumps(line_coverage)
